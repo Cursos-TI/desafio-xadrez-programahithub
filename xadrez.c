2b04a51444c29b861f5efe669385c48a20d91cc6
programacaohithub <macedogusmao@gmail.com>
2025-05-24 12:26:50 -0300

    Aceitando o desafio xadrez

--- xadrez.c
+++ xadrez.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-// Desafio de Xadrez - MateCheck
+// Desafio de Xadrez - MateCheck Aceitando o desafio de xadrez
+
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
