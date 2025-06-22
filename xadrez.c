6d00407cb26af931ffca41a7b4d3eb1fbe4d23f1
programacaohithub <macedogusmao@gmail.com>
2025-06-22 17:59:10 -0300

    atualizando commit

--- xadrez.c
+++ xadrez.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
+
 //Jogo Xadrez atual
-
 // Desafio de Xadrez - MateCheck Aceitando o desafio de xadrez
-
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
 int main() {
-
 
     // Movimento da Torre - 5 casas para a Direita
     // Estrutura de repetição: for
@@ -67,5 +65,3 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    return 0;
-}
